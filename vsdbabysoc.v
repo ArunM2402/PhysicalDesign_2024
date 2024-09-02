84712ee18c07803cb4a15252dcb0f2be8b355134
ArunM2402 <Arun.M004@iiitb.ac.in>
2024-09-02 21:19:48 +0530

    Create vsdbabysoc.v

---
+++ vsdbabysoc.v
@@ -0,0 +1,39 @@
+module vsdbabysoc (
+   output wire OUT,
+   //
+   input  wire reset,
+   //
+   input  wire VCO_IN,
+   input  wire ENb_CP,
+   input  wire ENb_VCO,
+   input  wire REF,
+   //
+   // input  wire VREFL,
+   input  wire VREFH
+);
+
+   wire clk_arun;
+   wire [9:0] RV_TO_DAC;
+
+   rvmyth dut(
+      .OUT(RV_TO_DAC),
+      .clk_arun(clk_arun),
+      .reset(reset)
+   );
+
+   avsdpll pll (
+      .CLK(clk_arun),
+      .VCO_IN(VCO_IN),
+      .ENb_CP(ENb_CP),
+      .ENb_VCO(ENb_VCO),
+      .REF(REF)
+   );
+
+   avsddac dac (
+      .OUT(OUT),
+      .D(RV_TO_DAC),
+      // .VREFL(VREFL),
+      .VREFH(VREFH)
+   );
+   
+endmodule
